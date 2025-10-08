a9401b6c6f7254637cca27318aceb80ab5d3155f
Bilal ahmad <bilal.ahmad@optimusalgos.com>
2025-10-08 12:52:06 +0500

    updates

--- models/expert.model.js
+++ models/expert.model.js
@@ -5,6 +5,7 @@ const expertSchema = new mongoose.Schema(
         name: { type: String, required: true, trim: true, },
         email: { type: String, required: true, unique:true },
         mobile_no: { type: String, required: true},
+        message: { type: String },
     },
     { timestamps: true, }
 );
