796c78bbe4f949b57a25d873b3fd832d8f4b3bd6
Bilal ahmad <bilal.ahmad@optimusalgos.com>
2025-11-24 16:35:32 +0500

    updated

--- models/project.model.js
+++ models/project.model.js
@@ -13,7 +13,8 @@ const projectSchema = new mongoose.Schema(
     city: { type: String, required: true },
     location: { type: String, required: true },
     plan_status: { type: String, required: true },
-    property_type: { type: String, required: true },
+    // property_type: { type: String, required: true },
+      property_type: [{ type: String, required: true }],
 
     about_overview: { type: String },
     about_points: [{ type: String }],
